10e15b827d0a677ba8b5a548814fd695f0b32101
not0ff <209936853+not0ff@users.noreply.github.com>
2024-12-17 22:03:11 +0100

    style the layout template and add  a simple navbar

--- src/app/core/routes.py
+++ src/app/core/routes.py
@@ -1,6 +1,22 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from . import core
 
 @core.route('/')
 def index():
-    return render_template('core/home.html')
+    return redirect(url_for('core.feed'))
+
+@core.route('/feed/')
+def feed():
+    return render_template('core/feed.html')
+
+@core.route('/newest/')
+def newest():
+    return render_template('core/newest.html')
+
+@core.route('/search/')
+def search():
+    return render_template('core/search.html')
+
+@core.route('/profile/')
+def profile():
+    return render_template('core/profile.html')
