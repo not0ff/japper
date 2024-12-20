3d8a3818d10aa583c1fb6330391af3c71b140e6b
not0ff <209936853+not0ff@users.noreply.github.com>
2024-12-21 00:25:08 +0100

    add validated login and signup forms

--- src/app/auth/routes.py
+++ src/app/auth/routes.py
@@ -1,15 +1,24 @@
 from flask import render_template, redirect, url_for, session, request
+from app.forms import LoginForm, SignupForm
 from . import auth
 
 
 @auth.route('/login/', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    form = LoginForm()
+    if form.validate_on_submit():
         session['logged'] = True
-
         return redirect(url_for('core.index'))
-    else:
-        return render_template('auth/login.html')
+    return render_template('auth/login.html', form=form)
+
+@auth.route('/signup/', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        session['logged'] = True
+        return redirect(url_for('core.index'))
+    return render_template('auth/signup.html', form=form)
+
 
 
 @auth.route('/logout/')
