4facc62048dc5a7ca66143ad3aeffbe09a458f0d
not0ff <209936853+not0ff@users.noreply.github.com>
2024-12-21 18:59:11 +0100

    small change to login and signup pages

--- src/app/forms.py
+++ src/app/forms.py
@@ -10,7 +10,7 @@ class SignupForm(FlaskForm):
         message='Enter a password'), Length(min=8, max=120, message='Password must have at least %(min)d characters')])
     confirm_password = PasswordField('Repeat password', description='Enter a matching password', validators=[
                                      DataRequired(message='Repeat your password'), EqualTo('password', message='Passwords must be equal')])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
@@ -18,4 +18,4 @@ class LoginForm(FlaskForm):
                            DataRequired(message='Enter a username')])
     password = PasswordField('Password', description='Provide a valid password', validators=[
                              DataRequired(message='Enter a password')])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Login')
